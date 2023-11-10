c4261bf847799d5aa33b88096cf3e563ad995111
unknown <you@example.com>
2023-11-10 15:24:14 +0100

    process

--- index.ts
+++ index.ts
@@ -7,3 +7,13 @@ const server = app.listen(port, ()=>{
     console.log("server is listening",port);
     
 })
+
+const App = express()
+const Port: number = 2000
+
+const Server = app.listen(port, ()=>{
+    console.log("server is listening",port);
+    
+})
+
+
